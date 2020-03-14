991e7791959866e5c75dbe1418f77abde250e512
Andy C. Wong <wongandy31@gmail.com>
2020-03-14 14:24:57 +0800

    created base

---
+++ application/views/item/index.php
@@ -0,0 +1,21 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>bootstrap/dist/css/bootstrap.min.css">
+</head>
+<body>
+	<div class="container">
+  
+	</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="<?php echo base_url(); ?>bootstrap/dist/js/bootstrap.min.js"></script>
+<script>
+$(document).ready(function() {
+	alert(3);
+});
+</script>
+</body>
+</html>
